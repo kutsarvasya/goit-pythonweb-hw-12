95ad6067c427c532b7ba0689ca27e4d6de260aba
Vasyl Kutsar <vkutsar89@gmail.com>
2026-05-16 22:05:29 +0200

    Integrate Brevo API and update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,22 +4,18 @@ import sys
 sys.path.insert(0, os.path.abspath("../.."))
 
 os.environ.setdefault(
-    "DB_URL", "postgresql+asyncpg://postgres:567234@localhost:5432/contacts_app"
+    "DB_URL",
+    "postgresql+asyncpg://postgres:567234@localhost:5432/contacts_app",
 )
 os.environ.setdefault("JWT_SECRET", "test_secret")
 os.environ.setdefault("JWT_ALGORITHM", "HS256")
 os.environ.setdefault("JWT_EXPIRATION_SECONDS", "3600")
 
-os.environ.setdefault("MAIL_USERNAME", "test@example.com")
-os.environ.setdefault("MAIL_PASSWORD", "test_password")
+os.environ.setdefault("BREVO_API_KEY", "test_brevo_api_key")
+
 os.environ.setdefault("MAIL_FROM", "test@example.com")
-os.environ.setdefault("MAIL_PORT", "465")
-os.environ.setdefault("MAIL_SERVER", "smtp.example.com")
 os.environ.setdefault("MAIL_FROM_NAME", "Contacts API")
-os.environ.setdefault("MAIL_STARTTLS", "False")
-os.environ.setdefault("MAIL_SSL_TLS", "True")
-os.environ.setdefault("USE_CREDENTIALS", "True")
-os.environ.setdefault("VALIDATE_CERTS", "False")
+
 os.environ.setdefault("REDIS_URL", "redis://localhost:6379")
 
 os.environ.setdefault("CLD_NAME", "test")
